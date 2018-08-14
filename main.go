61f9711b62c113efe4e1da9c65239a837550a020
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-08-14 14:47:32 +0100

    Read config from file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 	//"github.com/bwmarrin/discordgo"
 )
@@ -14,27 +15,41 @@ var (
 )
 
 func main() {
-	const input = "token=123123\nowner=monodokimes#1072"
-	reader := strings.NewReader(input)
+	file, err := os.Open("config.txt")
+	defer file.Close()
 
+	if err != nil {
+		log.Println("Config not found, creating...")
+
+		if _, err := file.WriteString("token=\nowner=\n"); err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("Please fill in the config file and run again")
+	}
+
+	reader := bufio.NewReader(file)
 	scanner := bufio.NewScanner(reader)
 
 	for scanner.Scan() {
 		text := scanner.Text()
 		values := strings.Split(text, "=")
-		for i := 0; i < 2; i++ {
-			key := values[0]
-			value := values[1]
 
-			switch key {
-			case "token":
-				Token = value
-			case "owner":
-				Owner = value
-			default:
-				fmt.Printf("unknown key %s", key)
-			}
+		key := values[0]
+		value := values[1]
+
+		switch key {
+		case "token":
+			Token = value
+		case "owner":
+			Owner = value
+		default:
+			fmt.Printf("unknown key '%s'\n", key)
 		}
+	}
+
+	if Token == "" || Owner == "" {
+		log.Fatalln("Config not set. Please fill in config.txt")
 	}
 
 	fmt.Printf("%s, %s\n", Owner, Token)
