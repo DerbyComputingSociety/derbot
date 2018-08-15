3ec2024aa04c96e552480b098e4980f496b6f15d
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-08-15 16:24:25 +0100

    Connect to Discord

--- main.go
+++ main.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"strings"
-	//"github.com/bwmarrin/discordgo"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
+
+const CONFIG_PATH = "config.txt"
 
 var (
 	Token string
@@ -15,7 +20,48 @@ var (
 )
 
 func main() {
-	file, err := os.Open("config.txt")
+	readConfig()
+
+	discord, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discord.AddHandler(messageCreate)
+
+	err = discord.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
+
+// Handle a message created on any channel the bot has access to
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	prefix := "::"
+
+	if strings.HasPrefix(m.Content, prefix) {
+		log.Printf(
+			"%s\t%s\t%s",
+			m.ChannelID,
+			m.Author.ID,
+			strings.TrimPrefix(m.Content, prefix))
+
+		s.ChannelMessageSend(m.ChannelID, "command used")
+	}
+}
+
+func readConfig() {
+	file, err := os.Open(CONFIG_PATH)
 	defer file.Close()
 
 	if err != nil {
@@ -52,10 +98,7 @@ func main() {
 		log.Fatalln("Config not set. Please fill in config.txt")
 	}
 
-	fmt.Printf("%s, %s\n", Owner, Token)
-
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-
 }
